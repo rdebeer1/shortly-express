37da44a835ae0d40b464e3488673cbb0fdd8234a
rdebeer1 <Robert.DeBeer@gmail.com>
2018-02-13 16:27:25 -0600

    completes v.1 of shortly sprint

--- app/config.js
+++ app/config.js
@@ -36,6 +36,18 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 20).unique;
+      user.string('password', 20);
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
