4260136e0aaba69d28e1fc428f7cbd4caaba3a75
rdebeer1 <Robert.DeBeer@gmail.com>
2018-02-13 16:31:16 -0600

    completes sprint

--- shortly.js
+++ shortly.js
@@ -2,15 +2,15 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
-
+var session = require('express-session')
+var cookieParser = require('cookie-parser');
 var db = require('./app/config');
 var Users = require('./app/collections/users');
 var User = require('./app/models/user');
 var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
-
+var bcrypt = require('bcrypt');
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -21,27 +21,33 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser('ahh'));
+app.use(session({secret: 'secret', resave: false, saveUninitialized: true}));
 
+var isLoggedIn = function(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
 
-app.get('/', 
-function(req, res) {
+app.get('/', isLoggedIn, function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
-function(req, res) {
-  res.render('index');
+app.get('/create', isLoggedIn, function(req, res) {
+  res.render('create');
 });
 
-app.get('/links', 
-function(req, res) {
+
+app.get('/links', isLoggedIn, function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.status(200).send(links.models);
   });
 });
 
-app.post('/links', 
-function(req, res) {
+app.post('/links', isLoggedIn, function(req, res) {
   var uri = req.body.url;
 
   if (!util.isValidUrl(uri)) {
@@ -63,8 +69,7 @@ function(req, res) {
           url: uri,
           title: title,
           baseUrl: req.headers.origin
-        })
-        .then(function(newLink) {
+        }).then(function(newLink) {
           res.status(200).send(newLink);
         });
       });
@@ -75,8 +80,64 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.post('/signup', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+  new User({ username: username }).fetch().then(function(found) {
+    if (found) {
+      console.log('user already exists!');
+      res.redirect('/login');
+    } else {
+      Users.create({
+        username: username,
+        password: hash
+      }).then(function(newUser) {
+        console.log('Username Added!', username, hash);
+        res.sendStatus(201);
+        res.redirect('/login');
+      });
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  new User({ username: username }).fetch().then(function(found) {
+    if (found) {
+      console.log('User found');
+      if (bcrypt.compareSync(password, found.get('password'))) {
+        console.log('hashed passwords match!');
+        req.session.user = username;
+        res.redirect('/');
+      } else {
+        console.log('Hashed passwords do not match!');
+        res.redirect('/login');
+      }
+    } else {
+      console.log('incorrect username or password');
+      res.redirect('/login');
+    }
+  });
+});
 
 
+
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+app.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.render('login');
+});
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
